8a7220ccb766ea9594773c2b05fc09ac2ee550bf
khushbu gupta code <mitalikhandelwal40@gmail.com>
2024-06-07 17:03:47 +0530

    javascript start

---
+++ index.js
@@ -0,0 +1,40 @@
+// var _mName = "vinod";
+// var _1m_name = 'bhadur';
+// var 1mName = 'thapa';
+// var $myName = 'thapa';
+// const x;
+// x = 3;
+// console.log(x)
+
+
+// let num = -3/0;
+// console.log(num)
+
+let num = 23n;
+console.log(num)
+console.log(typeof num)
+
+
+// conversion
+
+// let score = null;
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+// '33'  => 33
+// '33abc'  => NaN
+ // null => 0
+ // undefined => NaN
+ // true  => 1
+
+
+//  let logged = "add"
+//  let converter = Boolean(logged);
+//  console.log(converter)
+//  console.log(typeof converter)
+
+// let stringa = 33;
+// let converter = String(33)
+// console.log(converter)
+// console.log(typeof converter)
